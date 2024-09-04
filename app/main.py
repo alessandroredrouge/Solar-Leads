19f615708e16521749ba9a40058db4fd3b467005
alessandroredrouge <alessandroredrouge@gmail.com>
2024-09-04 12:24:33 +1000

    Updated the webpages structure, with an initial login page ('role_selection) that allows the user to choose between Canvasser and Manager. Once this choice has been done, different pages will be accessible to the user. Canvasser: Data Collection, Field Support. Manager: all 3

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
-# Purpose: Acts as the entry point for your application. It will set up a basic web server using Flask (a lightweight Python web framework). This server will handle HTTP requests for data collection and serve the frontend web app.
-# Interacts with: data_processing.py, google_maps.py, database.py, /templates/index.html.
+# Purpose: Acts as the entry point for your application. It sets up a web server using Flask, handles user role selection (Canvasser or Manager), and routes users to the appropriate interface. Canvassers are directed to the Data Collection page, while Managers are directed to the Analytics page. It also facilitates navigation between Data Collection, Field Support, and Analytics pages based on user roles.
+# Interacts with: data_processing.py, google_maps.py, database.py, /templates/role_selection.html, /templates/data_collection.html, /templates/field_support.html, /templates/analytics.html.
 # Programming Language: Python (Flask).
+
