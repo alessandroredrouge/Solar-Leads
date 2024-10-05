cfb8c26342e4d1064f5d112d2bbab79d335fc325
alessandroredrouge <alessandroredrouge@gmail.com>
2024-10-05 13:15:18 +1000

    Added a way to securely upload the code on Github and deploying the software on the web without leaking the Google Maps API key

--- build.js
+++ build.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const configPath = path.join(__dirname, 'static', 'config.js');
+const configPath = path.join(__dirname, 'app', 'static', 'config.js');
 let configContent = fs.readFileSync(configPath, 'utf8');
 
 configContent = configContent.replace('__GOOGLE_MAPS_API_KEY__', process.env.GOOGLE_MAPS_API_KEY || 'YOUR_LOCAL_API_KEY');
