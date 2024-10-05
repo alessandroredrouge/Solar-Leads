3c74ef001f7a7eac1d926f703f4db87898fbbd83
alessandroredrouge <alessandroredrouge@gmail.com>
2024-10-05 12:27:18 +1000

    Initial setup to introduce interaccctive maps in the software

---
+++ build.js
@@ -0,0 +1,10 @@
+// build.js
+const fs = require('fs');
+const path = require('path');
+
+const configPath = path.join(__dirname, 'static', 'config.js');
+let configContent = fs.readFileSync(configPath, 'utf8');
+
+configContent = configContent.replace('__GOOGLE_MAPS_API_KEY__', process.env.GOOGLE_MAPS_API_KEY || 'YOUR_LOCAL_API_KEY');
+
+fs.writeFileSync(configPath, configContent);
